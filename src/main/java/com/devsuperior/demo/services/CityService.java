aca4b5542d60d36a149e1e614048c93d5fc1abc6
mikael-ayala <mikael.ayala.santos@gmail.com>
2024-03-06 20:58:40 -0300

    add City findAll sorted by name

---
+++ src/main/java/com/devsuperior/demo/services/CityService.java
@@ -0,0 +1,25 @@
+package com.devsuperior.demo.services;
+
+import com.devsuperior.demo.dto.CityDTO;
+import com.devsuperior.demo.entities.City;
+import com.devsuperior.demo.repositories.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Comparator;
+import java.util.List;
+
+@Service
+public class CityService {
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    @Transactional(readOnly = true)
+    public List<CityDTO> findAll() {
+        List<City> cities = cityRepository.findAll();
+
+        return cities.stream().map(CityDTO::new).sorted(Comparator.comparing(CityDTO::getName)).toList();
+    }
+}
